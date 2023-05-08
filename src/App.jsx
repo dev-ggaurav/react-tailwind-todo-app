421766cac70a878af609280b81059bb8d2d6357c
devggaurav <devggaurav@gmail.com>
2023-05-08 12:22:38 +0530

    style change

--- src/App.jsx
+++ src/App.jsx
@@ -71,9 +71,9 @@ export default function App() {
               <li key={todo.id} className="task bg-slate-100 rounded-lg px-4 py-2 mt-2 flex items-center">
                 <input className="h-6 w-6" type="checkbox" checked = {todo.completed} onChange={ e => toggleTodo(todo.id, e.target.checked)} />
                 <p className="text-2xl grow ml-4 font-normal text-slate-950">{todo.title}</p>
-                <button onClick={ () => deleteTodo(todo.id)} class="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-rose-600 rounded-full focus:shadow-outline hover:bg-rose-700">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                <button onClick={ () => deleteTodo(todo.id)} className="inline-flex items-center justify-center w-8 h-8 mr-2 text-pink-100 transition-colors duration-150 bg-rose-600 rounded-full focus:shadow-outline hover:bg-rose-700">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                   </svg>
                 </button>
               </li>
